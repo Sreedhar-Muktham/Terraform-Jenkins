bab3a9cb0d903137079353ac7364b529ced1abc4
Sreedhar-Muktham <43406443+Sreedhar-Muktham@users.noreply.github.com>
2020-11-06 13:27:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region     = "us-east-1"
   profile    = "default" 
 }
+
+terraform {
+  backend "s3" {
+    bucket = "backend-sree1987"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
